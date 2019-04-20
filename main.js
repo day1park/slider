5dcab67d8d06958293bc7e340a498dd14352be5c
day1park <daypark2408@gmail.com>
2019-04-20 16:03:18 +1000

    create prev slide function

--- main.js
+++ main.js
@@ -21,3 +21,20 @@ const nextSlide = () => {
 
   setTimeout(() => current.classList.remove("current"));
 };
+
+const prevSlide = () => {
+  // will get current class
+  const current = document.querySelector(".current");
+  // will remove current class
+  current.classList.remove("current");
+  // check for prev slide
+  if (current.previousElementSibling) {
+    // add current to previous sibling if true
+    current.previousElementSibling.classList.add("current");
+  } else {
+    // add current to last sibling
+    slides[slides.length - 1].classList.add("current");
+  }
+
+  setTimeout(() => current.classList.remove("current"));
+};
