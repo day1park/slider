d5bfd6ed41b100cf5de5830a8e77cfd36cdd3cb3
day1park <daypark2408@gmail.com>
2019-04-20 16:01:34 +1000

    create next slide function

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ const prev = document.querySelector("#prev");
 const auto = false;
 const intervalTime = 5000;
 let slideInterval;
+
+const nextSlide = () => {
+  // will get current class
+  const current = document.querySelector(".current");
+  // will remove current class
+  current.classList.remove("current");
+  // check for next slide
+  if (current.nextElementSibling) {
+    // add current to next sibling if true
+    current.nextElementSibling.classList.add("current");
+  } else {
+    // add current to start if no next element sibling
+    slides[0].classList.add("current");
+  }
+
+  setTimeout(() => current.classList.remove("current"));
+};
