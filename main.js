969383ddc3dab87bbe170ee7b26f7211d9e326a5
day1park <daypark2408@gmail.com>
2019-04-20 15:57:42 +1000

    create variables for functions

---
+++ main.js
@@ -0,0 +1,6 @@
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
